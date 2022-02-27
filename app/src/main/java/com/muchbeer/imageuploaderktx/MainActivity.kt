4ab24df0a7c4faad7ff7e34b54d4e3d3875cfc49
muchbeer <gman2them@gmail.com>
2022-02-27 22:27:23 +0300

    Using coroutine launch io dispatcher worked like charm. Log to see the ping uSING gson to convert json to pojo Ignore api key and base uri from upload to the git Using very simple scouped storage to upload the data to the server

--- app/src/main/java/com/muchbeer/imageuploaderktx/MainActivity.kt
+++ app/src/main/java/com/muchbeer/imageuploaderktx/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.core.content.FileProvider
 import androidx.lifecycle.lifecycleScope
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.muchbeer.imageuploaderktx.api.Repository
 import com.muchbeer.imageuploaderktx.api.UploadInstance
 import com.muchbeer.imageuploaderktx.databinding.ActivityMainBinding
 import com.muchbeer.imageuploaderktx.util.UriFileConverter
@@ -32,7 +33,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
 
     private var selectedImageUri: Uri? = null
-   // private var selectedImageFile : File? = null
+    private var selectedImageFile : File? = null
 
    private val takePhoto = registerForActivityResult(ActivityResultContracts.TakePicture()) { isSuccess->
 
@@ -90,11 +91,12 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun getFileUri(fileName: String): Uri {
-        val tmpFile = File.createTempFile(fileName, ".jpg", cacheDir).apply {
+        val tmpFile = File.createTempFile(fileName, ".png", cacheDir).apply {
             createNewFile()
             deleteOnExit()
         }
-
+        Log.d(TAG, "tHE name of the file is :${tmpFile.absolutePath}")
+        selectedImageFile = tmpFile
         return FileProvider.getUriForFile(applicationContext, "${BuildConfig.APPLICATION_ID}.provider", tmpFile)
     }
 
@@ -107,56 +109,11 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
-        binding.progressBar.progress = 0
-        val parcelFileDescriptor =
-            contentResolver.openFileDescriptor(selectedImageUri!!, "r", null) ?: return
-
-
-        val inputStream = FileInputStream(parcelFileDescriptor.fileDescriptor)
-        val file = File(cacheDir, contentResolver.getFileName(selectedImageUri!!))
-     //   val file = getRealPathFromURI(this, selectedImageUri!!)
-        Log.d(TAG, "tHE filename to be sent is : ${file.absolutePath}")
-        val outputStream = FileOutputStream(file)
-        inputStream.copyTo(outputStream)
-
-        //option two
-        val mimeType = getMimeType(file)
-      //  val description_reqbody = description.toRequestBody("text/plain;charset=utf-8".toMediaType())
-        val descBody  = file.name.toRequestBody("text/plain;charset=utf-8".toMediaType())
-        val requestFileBody = file.asRequestBody(contentType = mimeType!!.toMediaTypeOrNull())
-
-       // val fileName = getRealPathFromURI(this, selectedImageUri!!)
+              // val fileName = getRealPathFromURI(this, selectedImageUri!!)
 
         lifecycleScope.launchWhenStarted {
-
-            val body = UploadRequestBody(file, "image") { percentage ->
-                binding.progressBar.progress = percentage
-            }
-
-            try {
-             val uploadImage =  UploadInstance.create().uploadSuspendImage(
-                    MultipartBody.Part.createFormData(
-                        name="image",
-                       filename= file.name,
-                       body = requestFileBody
-                    ),
-                    desc = descBody
-                )
-
-                if (uploadImage.isSuccessful) {
-                    uploadImage.body()!!.let {
-                        binding.layoutRoot.snackbar(it.message)
-                        binding.progressBar.progress = 100
-                        Log.d(TAG, "The json retrieved is : ${it.image}")
-                    }
-                } else {
-                    binding.layoutRoot.snackbar(uploadImage.message())
-                    binding.progressBar.progress = 0
-                    Log.d(TAG, "receive Error : ${uploadImage.message()}")
-                }
-            } catch (ex : IOException) {
-                Log.d(TAG, "eRROR IS : ${ex.message}")
-            }
+            Log.d(TAG, "The file is : ${selectedImageFile!!.absolutePath}")
+            Repository().okhttpRun(selectedImageFile)
         }
     }
 
